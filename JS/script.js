c99236032f2cd25ce2ea286f3087b1e88e169337
Ilya <Griczi@mail.ru>
2021-11-27 18:13:27 +0100

    work in js

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,17 @@
 'use strict';
 
-const numberOfFilms = prompt('Сколько фильмов вы уже просмотрели?', '10');
+const numberOfFilms = +prompt('Сколько фильмов вы уже просмотрели?', '10');
+
+if (numberOfFilms < 10) {
+  alert('мало');
+} else if (numberOfFilms <= 20) {
+  alert('норма');
+} else if (numberOfFilms >= 30) {
+  alert('ты гурман');
+} else {
+  alert('что то не так');
+}
+
 
 const personalMovieDB = {
   count: numberOfFilms,
@@ -10,16 +21,26 @@ const personalMovieDB = {
   privat: false,
 };
 
-let a = prompt('Один из последних просмотренных фильмов?', ''),
-  b = +prompt('На сколько оцените его?'),
-  c = prompt('Один из последних просмотренных фильмов?', ''),
-  d = +prompt('На сколько оцените его?');
+
+for (let i = 0; i < 2; i++) {
+  let a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = +prompt('На сколько оцените его?');
+
+  if (a !== null && b !== null && a !== '' && b !== '' && a.length < 20 && b <= 10) {
+    alert('Удалось!');
+    personalMovieDB.movies[a] = b;
+  } else {
+    alert('Не правильный ввод данных! Попробуй уще раз.');
+    i--;
+  }
+}
 
 
-// if (b || d < 0 && d || b > 10) {
-// prompt('Вы ввели не число или слишком большое!!');
-// }
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// if ((a && b == null)) {
+//   alert('Вы не можете проигнорировать эти вопросы');
+// }   //&& (a && b == str.length < 50)
+// } else if (a ) {
+
+
 
 console.log(personalMovieDB);
